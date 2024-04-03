77d3a2c0d320b4bd894c86c3a249c339c472fd1f
Darragh Ferry <darraghferry@hotmail.com>
2024-04-03 20:15:04 +0100

    json data now displays in routes component

--- a2ng_/src/app/routes/routes.component.ts
+++ a2ng_/src/app/routes/routes.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-routes',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './routes.component.html',
-  styleUrl: './routes.component.css'
+  styleUrl: './routes.component.css',
 })
 export class RoutesComponent {
+  jsonData: Object[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  onClick(url: string) {
+    this.http.get<Object[]>(url).subscribe((response) => {
+      this.jsonData = response;
+    });
+  }
 }
