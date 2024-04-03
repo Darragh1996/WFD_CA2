b57588f89238e1b61f99b8fd984bb9f06a4656eb
Darragh Ferry <darraghferry@hotmail.com>
2024-04-03 13:36:48 +0100

    routes partially set up

--- a2ng_/src/app/app.routes.ts
+++ a2ng_/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { RoutesComponent } from './routes/routes.component';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RoutesComponent, title: 'Routes' },
+  { path: 'routes', component: RoutesComponent, title: 'Routes' },
+];
