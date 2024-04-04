4c7d44665e85700ac3ffd191263dcc2868695b6b
Darragh Ferry <darraghferry@hotmail.com>
2024-04-04 22:31:34 +0100

    basic implementation of login implemented - still needs to redirect to admin

--- a2ng_/src/app/login/login.component.ts
+++ a2ng_/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,11 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   email = '';
   password = '';
+  loginURL = 'http://localhost:3000/login';
+
+  constructor(private http: HttpClient) {}
 
   onKeyUp(event: Event) {
-    console.log((event.target as HTMLSelectElement).id);
     if ((event.target as HTMLSelectElement).id == 'email') {
       this.email = (event.target as HTMLSelectElement).value;
     } else if ((event.target as HTMLSelectElement).id == 'password') {
@@ -20,5 +23,49 @@ export class LoginComponent {
     }
   }
 
-  onSubmit() {}
+  // onSubmit(event: Event) {
+  //   event.preventDefault();
+  //   this.http
+  //     .post(
+  //       this.loginURL,
+  //       { email: this.email, password: this.password },
+  //       { observe: 'response' }
+  //     )
+  //     .subscribe((response) => {
+  //       if (response.status == 200) {
+  //         console.log('login successful');
+  //       } else {
+  //         console.log('login failed');
+  //       }
+  //     });
+  // }
+
+  onSubmit(event: Event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    this.http
+      .post(
+        this.loginURL,
+        { email: this.email, password: this.password },
+        { observe: 'response' }
+      )
+      .subscribe(
+        (response) => {
+          // Handle successful response
+          if (response.status === 200) {
+            console.log('Login successful');
+            // Perform actions based on successful login, e.g., navigate to another page
+          } else {
+            // Handle other successful responses that might not be login success
+            console.log('Login failed');
+          }
+        },
+        (error) => {
+          // Handle error
+          console.error('Login error:', error);
+          // You can perform different actions based on the error status or type
+          // For instance, showing an error message to the user
+        }
+      );
+  }
 }
