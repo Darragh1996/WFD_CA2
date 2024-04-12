82f1043feae285515c60cddf84124fd9e655ec82
Darragh Ferry <darraghferry@hotmail.com>
2024-04-12 13:34:29 +0100

    clicking logout in navbar now removes isAdmin localstorage to log you out

--- a2ng_/src/app/nav/nav.component.ts
+++ a2ng_/src/app/nav/nav.component.ts
@@ -13,4 +13,10 @@ export class NavComponent {
   constructor(private localStorage: LocalStorageService) {}
 
   isAdmin = this.localStorage.getItem('adminLoggedIn');
+
+  logout(event: Event) {
+    this.isAdmin = false;
+    this.localStorage.removeItem('adminLoggedIn');
+    window.location.href = 'http://localhost:4200/login';
+  }
 }
