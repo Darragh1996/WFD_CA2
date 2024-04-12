cb2cfdc1077bedb80f4335fb37d4ccea95e32b58
Darragh Ferry <darraghferry@hotmail.com>
2024-04-12 17:24:33 +0100

    results now filterd by round buttons

--- a2ng_/src/app/results/results.component.ts
+++ a2ng_/src/app/results/results.component.ts
@@ -11,10 +11,31 @@ import { Result } from '../result';
 })
 export class ResultsComponent {
   results: Result[] = [];
+  displayResults: Result[] = [];
+  rounds: String[] = [];
+  currRoundIndex: number = 0;
 
   constructor(private resultsService: ResultsService) {
     this.resultsService.getResults().subscribe((response) => {
       this.results = response;
+      this.displayResults = response.filter((result) => {
+        return String(result.round) == '1';
+      });
+    });
+    this.resultsService.getRounds().subscribe((response) => {
+      this.rounds = response;
+    });
+  }
+
+  onClick(val: number) {
+    this.currRoundIndex = (this.currRoundIndex + val) % this.rounds.length;
+    if (this.currRoundIndex < 0) {
+      // to make sure it stays in range of the array
+      this.currRoundIndex = this.rounds.length - 1;
+    }
+
+    this.displayResults = this.results.filter((result) => {
+      return String(result.round) == String(this.rounds[this.currRoundIndex]);
     });
   }
 }
