874b30f24b3f311bed3a349f74a686f49dd53c0e
Darragh Ferry <darraghferry@hotmail.com>
2024-04-13 16:45:47 +0100

    added a footer component

--- a2ng_/src/app/app.component.ts
+++ a2ng_/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, RouterLink, RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    FooterComponent,
+    RouterLink,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
